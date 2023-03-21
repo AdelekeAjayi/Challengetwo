7d3b0943006afce368425f6b975baa6444d775dd
Adelekek2907 <123308017+Adelekek2907@users.noreply.github.com>
2023-03-21 16:41:57 +0100

    Initial Commit

---
+++ style.js
@@ -0,0 +1,15 @@
+const num2Handler = () => {
+  number2 = secondNum.value;
+};
+
+const addsHandler = () => {
+  result.innerHTML = parseInt(number1) + parseInt(number2);
+};
+
+const subHandler = () => {
+  result.innerHTML = parseInt(number1) - parseInt(number2);
+};
+
+const mulHandler = () => {
+  result.innerHTML = parseInt(number1) * parseInt(number2);
+};
